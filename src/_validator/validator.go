3f0628cec4f4310f9d22a8d2da112f58bbd77fa2
junyang7 <junyang7@staff.weibo.com>
2022-12-04 23:56:15 +0800

    重构：[_validator]包

--- src/_validator/validator.go
+++ src/_validator/validator.go
@@ -2,35 +2,155 @@ package _validator
 
 import (
 	"github.com/junyang7/go-common/src/_as"
+	"github.com/junyang7/go-common/src/_codeMessage"
+	"github.com/junyang7/go-common/src/_interceptor"
 	"github.com/junyang7/go-common/src/_is"
+	"strings"
 )
 
-type Validator struct {
-	name  string
-	value interface{}
+type Int struct {
+	name        string
+	value       int
+	codeMessage *_codeMessage.CodeMessage
 }
 
-func New(name string, value interface{}) *Validator {
-	return &Validator{
-		name:  name,
-		value: value,
+func NewInt(name string, value interface{}) *Int {
+	return &Int{
+		name:        name,
+		value:       _as.Int(value),
+		codeMessage: _codeMessage.ErrParameter,
 	}
 }
-func (this *Validator) Default(value interface{}) *Validator {
-	if _is.Empty(this.value) {
-		this.value = value
-	}
+func (this *Int) CodeMessage(codeMessage *_codeMessage.CodeMessage) *Int {
+	this.codeMessage = codeMessage
 	return this
 }
-func (this *Validator) Int() int {
-	return _as.Int(this.value)
+func (this *Int) Min(min int) *Int {
+	_interceptor.Insure(this.value >= min).
+		CodeMessage(this.codeMessage).
+		Data(map[string]interface{}{this.name: this.value}).
+		Do()
+	return this
 }
-func (this *Validator) String() string {
-	return _as.String(this.value)
+func (this *Int) Max(max int) *Int {
+	_interceptor.Insure(this.value <= max).
+		CodeMessage(this.codeMessage).
+		Data(map[string]interface{}{this.name: this.value}).
+		Do()
+	return this
 }
-func (this *Validator) Bool() bool {
-	return _as.Bool(this.value)
+func (this *Int) Between(min int, max int) *Int {
+	_interceptor.Insure(this.value >= min && this.value <= max).
+		CodeMessage(this.codeMessage).
+		Data(map[string]interface{}{this.name: this.value}).
+		Do()
+	return this
 }
-func (this *Validator) Float64() float64 {
-	return _as.Float64(this.value)
+func (this *Int) Value() int {
+	return this.value
+}
+
+type String struct {
+	name        string
+	value       string
+	codeMessage *_codeMessage.CodeMessage
+}
+
+func NewString(name string, value interface{}) *String {
+	return &String{
+		name:        name,
+		value:       strings.TrimSpace(_as.String(value)),
+		codeMessage: _codeMessage.ErrParameter,
+	}
+}
+func (this *String) CodeMessage(codeMessage *_codeMessage.CodeMessage) *String {
+	this.codeMessage = codeMessage
+	return this
+}
+func (this *String) NotEmpty() *String {
+	_interceptor.Insure(_is.NotEmpty(this.value)).
+		CodeMessage(this.codeMessage).
+		Data(map[string]interface{}{this.name: this.value}).
+		Do()
+	return this
+}
+func (this *String) MinLength(min int) *String {
+	_interceptor.Insure(len(this.value) >= min).
+		CodeMessage(this.codeMessage).
+		Data(map[string]interface{}{this.name: this.value}).
+		Do()
+	return this
+}
+func (this *String) MaxLength(max int) *String {
+	_interceptor.Insure(len(this.value) <= max).
+		CodeMessage(this.codeMessage).
+		Data(map[string]interface{}{this.name: this.value}).
+		Do()
+	return this
+}
+func (this *String) Value() string {
+	return this.value
+}
+
+type Bool struct {
+	name        string
+	value       bool
+	codeMessage *_codeMessage.CodeMessage
+}
+
+func NewBool(name string, value interface{}) *Bool {
+	return &Bool{
+		name:        name,
+		value:       _as.Bool(value),
+		codeMessage: _codeMessage.ErrParameter,
+	}
+}
+func (this *Bool) CodeMessage(codeMessage *_codeMessage.CodeMessage) *Bool {
+	this.codeMessage = codeMessage
+	return this
+}
+func (this *Bool) Value() bool {
+	return this.value
+}
+
+type Float64 struct {
+	name        string
+	value       float64
+	codeMessage *_codeMessage.CodeMessage
+}
+
+func NewFloat64(name string, value interface{}) *Float64 {
+	return &Float64{
+		name:        name,
+		value:       _as.Float64(value),
+		codeMessage: _codeMessage.ErrParameter,
+	}
+}
+func (this *Float64) CodeMessage(codeMessage *_codeMessage.CodeMessage) *Float64 {
+	this.codeMessage = codeMessage
+	return this
+}
+func (this *Float64) Min(min float64) *Float64 {
+	_interceptor.Insure(this.value >= min).
+		CodeMessage(this.codeMessage).
+		Data(map[string]interface{}{this.name: this.value}).
+		Do()
+	return this
+}
+func (this *Float64) Max(max float64) *Float64 {
+	_interceptor.Insure(this.value <= max).
+		CodeMessage(this.codeMessage).
+		Data(map[string]interface{}{this.name: this.value}).
+		Do()
+	return this
+}
+func (this *Float64) Between(min float64, max float64) *Float64 {
+	_interceptor.Insure(this.value >= min && this.value <= max).
+		CodeMessage(this.codeMessage).
+		Data(map[string]interface{}{this.name: this.value}).
+		Do()
+	return this
+}
+func (this *Float64) Value() float64 {
+	return this.value
 }
