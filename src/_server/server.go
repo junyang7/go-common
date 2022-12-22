7a8dbe7c16e69b360b9f351efaa02199e039d634
junyang7 <junyang7@staff.weibo.com>
2022-12-22 16:14:31 +0800

    增加：[_server]Web

--- src/_server/server.go
+++ src/_server/server.go
@@ -3,10 +3,14 @@ package _server
 import (
 	"github.com/junyang7/go-common/src/_server/_engine"
 	"github.com/junyang7/go-common/src/_server/_engine/_api"
+	"github.com/junyang7/go-common/src/_server/_engine/_web"
 )
 
 func Api(conf *_api.Conf) {
 	_api.Initialize(conf)
+}
+func Web(conf *_web.Conf) {
+	_web.Initialize(conf)
 }
 func Cli() {
 	e := &_engine.Cli{}
