091ee0bec326c6995efbf8f08f9702f1f8580a68
junyang7 <junyang7@staff.weibo.com>
2024-05-09 10:14:14 +0800

    _is包重构

--- _is/is.go
+++ _is/is.go
@@ -2,55 +2,52 @@ package _is
 
 import (
 	"fmt"
-	"strings"
+	"reflect"
 )
 
+// Empty 判断一个值是否是空
+// 支持：bool,int8,int16,int32,int64,int,uint8,uint16,uint32,uint64,uint,float32,float64,string,nil,[]x,map[x]x,[x]x,chan x
 func Empty(value interface{}) bool {
-	if `<nil>` == fmt.Sprintf("%v", value) {
-		return true
-	}
-	switch value.(type) {
-	case nil:
-		return true
-	case string:
-		return "" == strings.TrimSpace(value.(string))
-	case int8:
-		return 0 == value.(int8)
-	case int16:
-		return 0 == value.(int16)
-	case int32:
-		return 0 == value.(int32)
-	case int64:
-		return 0 == value.(int64)
-	case int:
-		return 0 == value.(int)
-	case uint8:
-		return 0 == value.(uint8)
-	case uint16:
-		return 0 == value.(uint16)
-	case uint32:
-		return 0 == value.(uint32)
-	case uint64:
-		return 0 == value.(uint64)
-	case float32:
-		return 0 == value.(float32)
-	case float64:
-		return 0 == value.(float64)
-	case bool:
+	var t string = fmt.Sprintf("%T", value)
+	switch t {
+	case `bool`:
 		return false == value.(bool)
-	case []byte:
-		return 0 == len(value.([]byte))
-	case map[string]string:
-		return 0 == len(value.(map[string]string))
-	case []map[string]string:
-		return 0 == len(value.([]map[string]string))
-	case [][]map[string]string:
-		return 0 == len(value.([][]map[string]string))
-	case []map[string]interface{}:
-		return 0 == len(value.([]map[string]interface{}))
-	case [][]map[string]interface{}:
-		return 0 == len(value.([][]map[string]interface{}))
-	default:
-		return false
+	case `int8`:
+		return 0 == value.(int8)
+	case `int16`:
+		return 0 == value.(int16)
+	case `int32`:
+		return 0 == value.(int32)
+	case `int64`:
+		return 0 == value.(int64)
+	case `int`:
+		return 0 == value.(int)
+	case `uint8`:
+		return 0 == value.(uint8)
+	case `uint16`:
+		return 0 == value.(uint16)
+	case `uint32`:
+		return 0 == value.(uint32)
+	case `uint64`:
+		return 0 == value.(uint64)
+	case `uint`:
+		return 0 == value.(uint)
+	case `float32`:
+		return 0 == value.(float32)
+	case `float64`:
+		return 0 == value.(float64)
+	case `string`:
+		return "" == value.(string)
+	case `<nil>`:
+		return true
 	}
+	v := reflect.ValueOf(value)
+	k := v.Kind().String()
+	switch k {
+	case `slice`, `map`, `chan`:
+		return v.Len() == 0
+	case `array`:
+		return v.IsZero()
+	}
+	return true
 }
