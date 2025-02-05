0a64c5925e4be540ae969c30554b2c2a9a9cbc69
junyang7 <junyang7@staff.weibo.com>
2025-02-05 20:54:00 +0800

    优化包：_xml

--- src/_xml/xml.go
+++ src/_xml/xml.go
@@ -2,9 +2,20 @@ package _xml
 
 import (
 	"encoding/xml"
+	"github.com/junyang7/go-common/src/_as"
 	"github.com/junyang7/go-common/src/_interceptor"
 )
 
+func Encode(data interface{}) []byte {
+	b, err := xml.Marshal(data)
+	if nil != err {
+		_interceptor.Insure(false).Message(err).Do()
+	}
+	return b
+}
+func EncodeAsString(data interface{}) string {
+	return _as.String(Encode(data))
+}
 func Decode(source []byte, target interface{}) {
 	if err := xml.Unmarshal(source, target); nil != err {
 		_interceptor.Insure(false).Message(err).Do()
