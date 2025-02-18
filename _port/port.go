bbdbc7358921275f4c19cd5d49b275be3833fc92
junyang7 <junyang7@staff.weibo.com>
2025-02-18 16:58:36 +0800

    [_port]：①增加过滤；②增加实例对象；

--- _port/port.go
+++ _port/port.go
@@ -2,21 +2,72 @@ package _port
 
 import (
 	"fmt"
+	"github.com/junyang7/go-common/_list"
 	"os/exec"
 )
 
-func GetList(count int) []int {
-	var portList []int
-	var i int
-	for i = 10000; i < 65535; i++ {
-		handler := exec.Command(`lsof`, fmt.Sprintf(`-i:%d`, i))
-		_, err := handler.Output()
-		if err != nil {
-			portList = append(portList, i)
-			if len(portList) >= count {
+func IsUsing(port int) bool {
+	handler := exec.Command(`lsof`, fmt.Sprintf(`-i:%d`, port))
+	_, err := handler.Output()
+	return err != nil
+}
+func GetList(count int) (o []int) {
+	for i := 1; i <= 65535; i++ {
+		if !IsUsing(i) {
+			o = append(o, i)
+			if len(o) >= count {
 				break
 			}
 		}
 	}
-	return portList
+	return o
+}
+func Get() (o int) {
+	return GetList(1)[0]
+}
+
+type port struct {
+	min    int
+	max    int
+	count  int
+	filter []int
+}
+
+func New() *port {
+	return &port{
+		min:    20000,
+		max:    65535,
+		count:  10,
+		filter: []int{},
+	}
+}
+func (this *port) Min(min int) *port {
+	this.min = min
+	return this
+}
+func (this *port) Max(max int) *port {
+	this.max = max
+	return this
+}
+func (this *port) Count(count int) *port {
+	this.count = count
+	return this
+}
+func (this *port) Filter(filter []int) *port {
+	this.filter = append(this.filter, filter...)
+	return this
+}
+func (this *port) GetList() (o []int) {
+	for i := this.min; i <= this.max; i++ {
+		if _list.In(i, this.filter) {
+			continue
+		}
+		if !IsUsing(i) {
+			o = append(o, i)
+			if len(o) >= this.count {
+				break
+			}
+		}
+	}
+	return o
 }
