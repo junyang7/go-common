9e4dd677432af372a839ca16448ef01839e0ffcd
junyang7 <junyang7@staff.weibo.com>
2025-03-04 23:11:01 +0800

    [AI优化]：_cache

--- _cache/cache.go
+++ _cache/cache.go
@@ -7,30 +7,30 @@ import (
 
 type cache struct {
 	v interface{}
-	t time.Duration
+	t time.Time
 }
 
-var cacheDict map[string]*cache = map[string]*cache{}
-var m sync.RWMutex = sync.RWMutex{}
+var cacheDict sync.Map
+var m sync.RWMutex
 
-func SetByMemory(k string, v interface{}, t time.Duration) bool {
-	if m.TryLock() {
-		defer m.Unlock()
-		cacheDict[k] = &cache{
-			v: v,
-			t: t,
-		}
-		return true
-	}
-	return false
+func SetByMemory(k string, v interface{}, ttl time.Duration) bool {
+	cacheDict.Store(k, &cache{
+		v: v,
+		t: time.Now().Add(ttl),
+	})
+	return true
 }
 func GetByMemory(k string) interface{} {
-	if m.TryRLock() {
-		defer m.RUnlock()
-		v, ok := cacheDict[k]
-		if ok {
-			return v
+	if v, ok := cacheDict.Load(k); ok {
+		c := v.(*cache)
+		if time.Now().After(c.t) {
+			cacheDict.Delete(k)
+			return nil
 		}
+		return c.v
 	}
 	return nil
+}
+func DelByMemory(k string) {
+	cacheDict.Delete(k)
 }
