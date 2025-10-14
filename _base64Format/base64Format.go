3887810deb42dc85f7d42489e6a1963a3d5d78a3
junyang7 <junyang7@staff.weibo.com>
2025-10-14 17:09:14 +0800

    v1.0.4

--- _base64Format/base64Format.go
+++ _base64Format/base64Format.go
@@ -10,8 +10,6 @@ func Encode(data string) string {
 }
 func Decode(data string) string {
 	b, err := base64.RawURLEncoding.DecodeString(data)
-	if nil != err {
-		_interceptor.Insure(false).Message(err).Do()
-	}
+	_interceptor.Insure(nil == err).Message(err).Do()
 	return string(b)
 }
