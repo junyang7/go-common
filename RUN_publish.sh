44c95673bcca96c07f79a93096f1b31a3ef387b5
junyang7 <junyang7@staff.weibo.com>
2026-03-06 14:58:09 +0800

    1.2.20260306145809

--- RUN_publish.sh
+++ RUN_publish.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-PATH="./VERSION"
+FILE="./VERSION"
 TIMESTAMP="$(/bin/date +%Y%m%d%H%M%S)"
-VERSION="$(/bin/cat "$PATH")"
+VERSION="$(/bin/cat "$FILE")"
 IFS='.' read -r MAJOR MINOR PATCH <<< "$VERSION"
 NEW_VERSION="${MAJOR}.${MINOR}.${TIMESTAMP}"
-echo "$NEW_VERSION" > "$PATH"
+echo "$NEW_VERSION" > "$FILE"
 
 export https_proxy=http://127.0.0.1:7897 http_proxy=http://127.0.0.1:7897 all_proxy=socks5://127.0.0.1:7897
-/usr/bin/git add .
-/usr/bin/git commit -m "$NEW_VERSION"
-/usr/bin/git push
+git add .
+git commit -m "$NEW_VERSION"
+git push
